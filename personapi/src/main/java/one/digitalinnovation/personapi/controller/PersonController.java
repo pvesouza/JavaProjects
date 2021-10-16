68c07efc5bc2857a01f37657a57dc554697b0bd0
pvesouza <pvesouza@gmail.com>
2021-10-15 22:49:49 -0300

    save person on database

--- personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ personapi/src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,6 +1,11 @@
 package one.digitalinnovation.personapi.controller;
 
+import one.digitalinnovation.personapi.dto.MessageResponseDto;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/people")
 public class PersonController {
 
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @GetMapping
-    public String getBook(){
-        return "Te amo janaina";
+    public MessageResponseDto createPerson(@RequestBody Person person)
+    {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDto.builder().message("Created Person with Id " + savedPerson.getId()).build();
+
     }
 }
